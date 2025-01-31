ca10570dbf83e4f10acbacb0cb72c63f5a0132a7
aryan083 <aryanmahida2@gmail.com>
2025-01-31 22:50:29 +0530

    trying

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [React()],
   server: {
     port: 3000,
     open: true,
@@ -12,5 +8,5 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
-  base:'/potfolio/'
+  base: '/potfolio/'
 })
